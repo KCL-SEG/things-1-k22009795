49cbfdebe154561cb5bf9f94a18bfddba6324b16
Malik Khan <malik.khan@kcl.ac.uk>
2023-10-24 18:42:52 +0100

    Task 3

--- things/models.py
+++ things/models.py
@@ -2,9 +2,34 @@ from django.db import models
 from django.db.models import Model
 
 
+
 # Create your models here.
 
+from django.core.validators import RegexValidator, MaxValueValidator, MinValueValidator
+
 class Thing(models.Model):
-    name = models.CharField(max_length=20)
-    description = models.CharField(max_length=40)
-    quantity = models.IntegerField()
+    name = models.CharField(
+        max_length=30,
+        unique=True,
+        blank=False,
+        validators=[RegexValidator(
+            regex=r'^\w{1,30}$',
+            message='Username must be unique, not blank, and have no more than 30 characters'
+        )]
+    )
+    description = models.CharField(
+        max_length=120,
+        unique=False,
+        blank=True,
+        validators=[RegexValidator(
+            regex=r'^.{0,120}$',
+            message='Description can have up to 120 characters'
+        )]
+    )
+    quantity = models.IntegerField(
+        unique=False,
+        validators=[
+            MaxValueValidator(100, message='Quantity cannot be more than 100'),
+            MinValueValidator(0, message='Quantity cannot be less than 0')
+        ]
+    )
--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.core.exceptions import ValidationError
+from .models import Thing
+
+class ThingModelTest(TestCase):
+    def test_name_max_length(self):
+        # Test that the name field has a maximum length of 20 characters.
+        thing = Thing(name="A" * 21, description="Valid description", quantity=50)
+        with self.assertRaises(ValidationError):
+            thing.full_clean()
+
+    def test_name_unique(self):
+        # Test that the name field is unique.
+        thing1 = Thing(name="DuplicateName", description="Valid description", quantity=50)
+        thing1.save()
+        thing2 = Thing(name="DuplicateName", description="Another description", quantity=75)
+        with self.assertRaises(ValidationError):
+            thing2.full_clean()
+
+    def test_description_max_length(self):
+        # Test that the description field has a maximum length of 120 characters.
+        thing = Thing(name="ValidName", description="B" * 121, quantity=50)
+        with self.assertRaises(ValidationError):
+            thing.full_clean()
+
+    def test_quantity_min_max_values(self):
+        # Test that the quantity field has minimum and maximum value constraints.
+        # Valid value: 50
+        valid_thing = Thing(name="ValidName", description="Valid description", quantity=50)
+        valid_thing.full_clean()  # Should not raise an exception
+
+        # Invalid values: less than 0, more than 100
+        invalid_thing1 = Thing(name="InvalidName1", description="Invalid description", quantity=-1)
+        invalid_thing2 = Thing(name="InvalidName2", description="Invalid description", quantity=101)
+        with self.assertRaises(ValidationError):
+            invalid_thing1.full_clean()
+            invalid_thing2.full_clean()
+            
+    
